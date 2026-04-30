c6a226bd78bb8b4188bf92742fdd51cfad91f3da
Nik Rai <nikhilrr2311@gmail.com>
2026-04-30 23:41:13 +0200

    chore: update producer

---
+++ producer/src/main.py
@@ -0,0 +1,9 @@
+import argparse
+import os
+from uuid import uuid4
+
+from six.moves import input
+from confluent_kafka import Producer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
+from confluent_kafka.serialization import MessageField, SerializationContext, StringSerializer
