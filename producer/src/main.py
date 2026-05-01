5524ee24f95942eec6c7dbabc8302ac575cb62af
Nik Rai <nikhilrr2311@gmail.com>
2026-05-01 19:57:33 +0200

    producer config

--- producer/src/main.py
+++ producer/src/main.py
@@ -7,3 +7,11 @@ from confluent_kafka import Producer
 from confluent_kafka.schema_registry import SchemaRegistryClient
 from confluent_kafka.schema_registry.avro import AvroSerializer
 from confluent_kafka.serialization import MessageField, SerializationContext, StringSerializer
+
+# Configuration 
+
+BROKER = os.getenv("BROKER", "localhost:9092")
+SCHEMA_REGISTRY_URL = os.getenv("SCHEMA_REGISTRY_URL", "http://localhost:18081")
+TOPIC = os.getenv("topic", "orders.v1")
+SCV_PATH = os.getenv("schema_path", "schemas/order_item.v1.avsc")
+
